9f61728fc218de36e36d2abeb5437ec8e7916c25
WellingtonDeLacerda <wellingtonlacerda1515@gmail.com>
2025-12-16 04:55:35 -0300

    Atualizando

--- exercicios/ex024/script.js
+++ exercicios/ex024/script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector("#btn")
 const res = document.querySelector("#res")
 btn.addEventListener("click", () => {
     let pas = ""
-    res.innerHTML = "<h2>Contando de 1 até 10 e mostrando so os pares</h2><br>"
+    res.innerHTML = "<h2>Contagem regressiva de 10 até 1</h2><br>"
     for (let c = 10; c >= 1; c--) {
         if (c != 1) {
             pas = " 👉 "
