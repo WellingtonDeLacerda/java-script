59f8cce7f338567d277a9b28c3888111bae69284
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-08-31 14:40:32 -0300

    if else

---
+++ conceitos-completos/03-estrutura-condicional/01-if-else/index.js
@@ -0,0 +1,21 @@
+// estrutura condicional if
+
+// if (expressão) {}
+
+const temperature = 30
+
+if (temperature >= 30) {
+  console.log("Esta muito quente la fora")
+}else {
+  console.log("A temperatura está agradavel")
+}
+
+const grade = 7.5
+
+if (grade >= 9) {
+  console.log("Aprovado")
+}else if (grade >= 7) {
+  console.log("Aprovado na media")
+}else {
+  console.log("Reprovado estude mais na proxima")
+}
