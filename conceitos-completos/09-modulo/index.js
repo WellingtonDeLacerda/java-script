6545494956bf8d27fbdcbe24ad40e0e566479f7e
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-09-09 13:37:41 -0300

    modulo

---
+++ conceitos-completos/09-modulo/index.js
@@ -0,0 +1,8 @@
+import math from "./math/math.js"
+
+const { sum, sub, mult, div } = math
+
+console.log(sum(5,8))
+console.log(sub(5,8))
+console.log(mult(5,8))
+console.log(div(5,8))
---
+++ conceitos-completos/09-modulo/math/math.js
@@ -0,0 +1,13 @@
+function sum(a=0,b=0) {
+  return a + b
+}
+function sub(a=0,b=0) {
+  return a - b
+}
+function mult(a=0,b=0) {
+  return a * b
+}
+function div(a=0,b=0) {
+  return a / b
+}
+export default { sum, sub, mult, div }
